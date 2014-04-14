2cd0159a961f9d47439c1e6cc21d066d7d83d244
pp-mo <patrick.peglar@metoffice.gov.uk>
2014-04-14 01:33:08 +0100

    Begin coding main 'complete' operation (not tested).

--- lib/ncobj/grouping.py
+++ lib/ncobj/grouping.py
@@ -18,42 +18,22 @@ def walk_group_objects(group, of_types=None):
         walk_group_objects(subgroup, of_types)
 
 
+def all_variables(group):
+    return list(walk_group_objects(group, nco.Variable))
+
+
+def all_dimensions(group):
+    return list(walk_group_objects(group, nco.Dimensions))
+
+
+def all_groups(group):
+    return list(walk_group_objects(group, nco.Group))
+
+
 def find_group_root(group):
     while group.parent_group:
         group = group.parent_group
     return group
-
-
-def _find_definition(group, name, container_prop_name):
-    """
-    Search groups upward for a definition by name and container property name.
-
-    Args:
-    * group (:class:`Group`):
-        The group to start searching at.
-
-    * name (:class:`NcObj`):
-        The name the element should have.
-
-    * container_prop_name:
-        The Group container property to look in.
-
-    Returns:
-        An existing definition object, or None.
-
-    """
-    for element in getattr(group, container_prop_name):
-        if element.name == name:
-            # Found in given group.
-            return element
-
-    # Not in this group:  Look in the parent (if any).
-    if group.parent_group:
-        return _find_definition(group.parent_group, name,
-                                container_prop_name)
-
-    # Group has no parent, so we are done (fail).
-    return None
 
 
 def find_named_definition(group, name, element_type):
@@ -155,3 +135,114 @@ def resolve_all_references(group, create_missing_in_group=None):
             # Note: we must use a low-level assignment to insert 'dim'
             # itself, rather than a detached copy of it.
             var.dimensions.setitem_reference(dim.name, dim_definition)
+
+
+def group_path(ncobj):
+    path = ncobj.name
+    if ncobj.container and isinstance(ncobj.container.in_element, nco.Group):
+        path = group_path(ncobj.container) + '/' + path
+    return ncobj.name
+
+
+class DimensionConflictError(Exception):
+    pass
+
+
+class NameConflictError(Exception):
+    pass
+
+
+# Check that all names within a group are compatible.
+def check_group_name_clashes(group):
+    vv, dd, gg = ('variable', 'dimension', 'group')
+    def check_one_group(grp):
+        for type1, type2 in ((vv, dd), (vv, gg), (dd, gg)):
+            names1 = getattr(group, type1+'s').names()
+            names2 = getattr(group, type2+'s').names()
+            clashes = set(names1) & set(names2)
+            if clashes:
+                badname = list(clashes)[0]
+                raise NameConflictError('group "{}" contains both a {} and a '
+                                        '{} named {}.'.format(
+                                            group_path(grp), type1, type2,
+                                            badname)
+
+    for grp in all_groups(group):
+        check_one_group(grp)
+
+
+def complete(group):
+    # Link all variable dimensions to definitions.
+    # Resolve all dimensions to match variable sizes.
+    # Check cross-class names.
+    # NOTE: make dimensions unlimited when required, and also allow these to be
+    # used where the variable information implies a fixed dimension.
+    
+    # Assign temporary information to all dimensions, for recording usage.
+        #
+        # NOTE: may be better to add/remove with separate util methods.
+        #  THEN separately check + calculate everything ?
+        #  ALSO means check + calc _unlim separately (_really_ not hard).
+        #
+    for dim in all_dimensions(groups)
+        dim._vars, dim._len, dim._unlim = [], None, False
+
+    # Find or create definitions for the dimensions required by all variables.
+    new_created_dims = []
+    for var in all_variables(group):
+        for dim in var.dimensions:
+            # Locate existing dimension in structure, if any.
+            dim_def = find_named_definition(group, dim.name, nco.Dimension)
+            if dim_def is None:
+                # Create a new top-level dimension definition.
+                group.dimensions.add(dim)
+                dim_def = group.dimensions[dim.name]
+                dim_def._vars, dim_def._len, dim_def._unlim = [], None, False
+                # Keep a list of these, so we can remove again on error.
+                new_created_dims.append(dim_def)
+            # Record this variable as using this dimension
+            dim_def._vars.append((var, dim))
+
+    # Do consistency checks (backing out all changes on failure).
+    try:
+        # Check that the requirements for all dimensions are consistent.
+        for dim in all_dimensions(group):
+            if dim._vars:
+                var1, dim1 = dim._vars[0]
+                need_unlimited = dim1.unlimited
+                for (varx, dimx) in dim._vars[1:]:
+                    need_unlimited |= dimx.unlimited
+                    if dimx.length != dim1.length:
+                        # 
+                        raise DimensionConflictError(
+                            'Variable "{}" requires dimension "{}={}", but '
+                            'variable "{}" requires "{}={}"'.format(
+                                group_path(var1), dim1.name, dim1.length,
+                                group_path(varx), dimx.name, dimx.length))
+                dim._len, dim._unlim = dim1.length, need_unlimited
+
+        # Check for any name conflicts.
+        check_name_clashes(group)
+    except Exception as error:
+        # Back out changes, to leave passed argument as it was.
+        group.dimensions.remove_allof(new_created_dims)
+        for dim in all_dimensions(group):
+            del dim._vars, dim._len, dim._unlim
+        raise error
+
+    # Finally, make the actual structural changes.
+
+    # Run through all dimension definitions, fixing the required properties.
+    for dim in all_dimensions(group):
+        if dim._vars:
+            # NOTE: do not fiddle with any unused dimensions here.
+            # Can easily prune these if wanted.
+            dim.length, dim.unlimited = dim._length_unlim
+        del dim._vars, dim._len, dim._unlim
+
+    # Run through all the variables, fixing pointers to the definitions.
+    for var in all_variables(group):
+        for i_dim, dim in enumerate(var.dimensions):
+            dim_def = find_named_definition(group, dim.name, nco.Dimension)
+            assert dim_def is not None
+            var.dimensions[i_dim] = dim_def
