b2c1035fec355c003796dd736acf9c2bb899b0bf
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2014-04-15 14:08:16 +0100

    Tidied test_groupings.

--- lib/ncobj/tests/grouping/test_grouping.py
+++ lib/ncobj/tests/grouping/test_grouping.py
@@ -10,50 +10,9 @@ from ncobj.grouping import find_named_definition as fnd
 
 
 class MixinTestSetup(tests.TestCase):
-    def setUp(self):
-        self.subgroup = nco.Group()
-        self.all_objs = [self.subgroup]
-        self.sub_att_1 = nco.Attribute('att_1', value=4.3)
-        self.sub_att_2 = nco.Attribute('att_2', value=4.7)
-        self.subgroup.attributes.add(self.sub_att_1)
-        self.subgroup.attributes.add(self.sub_att_2)
-        self.all_objs += [self.sub_att_1, self.sub_att_2]
-        self.dim_sub_y = nco.Dimension('y', length=2)
-        self.dim_sub_x = nco.Dimension('x', length=3)
-        self.subgroup.dimensions.add(self.dim_sub_x)
-        self.subgroup.dimensions.add(self.dim_sub_y)
-        self.all_objs += [self.dim_sub_x, self.dim_sub_y]
-        self.var_sub_A = nco.Variable('var_A',
-                                      dimensions=(self.dim_sub_y,
-                                                  self.dim_sub_x),
-                                      dtype=float,
-                                      data=[[1, 2, 3], [4, 5, 6]])
-        self.attr_sub_var_A_A = nco.Attribute('var_att_a', value='this')
-        self.var_sub_A.attributes.add(self.attr_sub_var_A_A)
-        self.subgroup.variables.add(self.var_sub_A)
-        self.all_objs += [self.var_sub_A]
-        # NO! : self.all_objs += [self.attr_sub_var_A_A]
-
-        self.dim_sub_var_B_t = nco.Dimension('t', 5)
-        self.var_sub_var_B = nco.Variable('var_B',
-                                          dimensions=[self.dim_sub_var_B_t],
-                                          dtype=float,
-                                          data=[])
-        self.subgroup.variables.add(self.var_sub_var_B)
-        # NO! : self.all_objs += [self.dim_sub_var_B_t]
-        self.all_objs += [self.var_sub_var_B]
-
-        self.root = nco.Group()
-        self.all_objs += [self.root]
-        self.root.groups.setitem_reference('group_X', self.subgroup)
-        self.attr_A = nco.Attribute('test_global_attr', 'test string')
-        self.root.attributes['test_global_attr'] = self.attr_A
-        self.all_objs += [self.attr_A]
-        self.dim_T = nco.Dimension('t', 3)
-        self.root.dimensions.add(self.dim_T)
-        self.var_X = nco.Variable('x')
-        self.root.variables.add(self.var_X)
-        self.all_objs += [self.var_X, self.dim_T]
+    def check_all_in_str(self, str, matches):
+        for match in matches:
+            self.assertIn(match, str)
 
 
 def og(name, aa=None, dd=None, vv=None, gg=None):
@@ -77,10 +36,9 @@ def ov(name, dd=None, aa=None):
                         dtype=None, data=[])
 
 
-class Test__simple_maker(MixinTestSetup):
-    # A quick test of our reduced-form structure builders
-    def test__simple_maker(self):
-        test_root = og('',
+class Test_walk_group_objects(MixinTestSetup):
+    def setUp(self):
+        self.root = og('',
                        aa=[oa('test_global_attr', v='test string')],
                        dd=[od('t', 3)],
                        vv=[ov('x')],
@@ -90,19 +48,27 @@ class Test__simple_maker(MixinTestSetup):
                                      aa=[oa('var_att_a', 'this')]),
                                   ov('var_B', dd=[od('t')])],
                               aa=[oa('att_1', 4.3), oa('att_2', 4.7)])])
-#        print '\nTEST:\n', test_root
-#        print '\nSELF:\n', self.root
-        self.assertEqual(str(test_root), str(self.root))
+        subgroup = self.root.groups['group_X']
+        self.all_objs = [
+            self.root,
+            self.root.attributes['test_global_attr'],
+            self.root.dimensions['t'],
+            self.root.variables['x'],
+            subgroup,
+            subgroup.attributes['att_1'],
+            subgroup.attributes['att_2'],
+            subgroup.dimensions['x'],
+            subgroup.dimensions['y'],
+            subgroup.variables['var_A'],
+            subgroup.variables['var_B']]
 
-
-class Test_walk_group_objects(MixinTestSetup):
     def test_basic(self):
         walk_objs = list(walk_group_objects(self.root))
         # NOTE: can't use set-equality here, as it test 'is' not '__eq__'.
         for elem in self.all_objs:
             self.assertIn(elem, walk_objs)
         for obj in walk_objs:
-            self.assertIn(obj, walk_objs)
+            self.assertIn(obj, self.all_objs)
 
     def _check_all_of_type(self, walk_objs, elem_type):
         for elem in self.all_objs:
@@ -112,7 +78,7 @@ class Test_walk_group_objects(MixinTestSetup):
                 self.assertNotIn(elem, walk_objs)
         for obj in walk_objs:
             self.assertIsInstance(obj, elem_type)
-            self.assertIn(obj, walk_objs)
+            self.assertIn(obj, self.all_objs)
 
     def test_all_variables(self):
         walk_objs = list(all_variables(self.root))
@@ -145,18 +111,14 @@ class Test_group_path(MixinTestSetup):
 
 
 class Test_find_named_definition(MixinTestSetup):
-    def check_all_in_str(self, str, matches):
-        for match in matches:
-            self.assertIn(match, str)
-
     def test_type_bad(self):
         g = og('')
         with self.assertRaises(ValueError) as err_context:
             fnd(g, 'name', type(None))
         msg = err_context.exception.message
         self.check_all_in_str(msg, ['<type \'NoneType\'>',
-                              'not recognised',
-                              'or not supported'])
+                                    'not recognised',
+                                    'or not supported'])
 
     def test_type_unsupported(self):
         g = og('')
@@ -164,8 +126,8 @@ class Test_find_named_definition(MixinTestSetup):
             fnd(g, 'name', nco.Variable)
         msg = err_context.exception.message
         self.check_all_in_str(msg, ['ncobj.Variable',
-                              'not recognised',
-                              'or not supported'])
+                                    'not recognised',
+                                    'or not supported'])
 
     def test_root_none(self):
         g = og('')
