f65c1d54da655267d5a6cdb8e9b9075f39a8767d
pp-mo <patrick.peglar@metoffice.gov.uk>
2014-04-15 09:14:09 +0100

    More grouping tests, working.

--- lib/ncobj/tests/grouping/test_grouping.py
+++ lib/ncobj/tests/grouping/test_grouping.py
@@ -3,9 +3,10 @@ import unittest as tests
 
 import ncobj as nco
 
-from ncobj.grouping import \
-    walk_group_objects, all_variables, all_dimensions, all_groups, \
-    group_path
+from ncobj.grouping import walk_group_objects
+from ncobj.grouping import all_variables, all_dimensions, all_groups
+from ncobj.grouping import group_path
+from ncobj.grouping import find_named_definition as fnd
 
 
 class MixinTestSetup(tests.TestCase):
@@ -32,7 +33,7 @@ class MixinTestSetup(tests.TestCase):
         self.subgroup.variables.add(self.var_sub_A)
         self.all_objs += [self.var_sub_A]
         # NO! : self.all_objs += [self.attr_sub_var_A_A]
-        
+
         self.dim_sub_var_B_t = nco.Dimension('t', 5)
         self.var_sub_var_B = nco.Variable('var_B',
                                           dimensions=[self.dim_sub_var_B_t],
@@ -54,10 +55,50 @@ class MixinTestSetup(tests.TestCase):
         self.root.variables.add(self.var_X)
         self.all_objs += [self.var_X, self.dim_T]
 
-        
+
+def og(name, aa=None, dd=None, vv=None, gg=None):
+    return nco.Group(name,
+                     dimensions=dd,
+                     variables=vv,
+                     attributes=aa,
+                     sub_groups=gg)
+
+
+def oa(name, v=None):
+    return nco.Attribute(name, value=v)
+
+
+def od(name, l=None, u=False):
+    return nco.Dimension(name, length=l, unlimited=u)
+
+
+def ov(name, dd=None, aa=None):
+    return nco.Variable(name, dimensions=dd, attributes=aa,
+                        dtype=None, data=[])
+
+
+class Test__simple_maker(MixinTestSetup):
+    # A quick test of our reduced-form structure builders
+    def test__simple_maker(self):
+        test_root = og('',
+                       aa=[oa('test_global_attr', v='test string')],
+                       dd=[od('t', 3)],
+                       vv=[ov('x')],
+                       gg=[og('group_X',
+                              dd=[od('x', 3), od('y', 2)],
+                              vv=[ov('var_A', dd=[od('y'), od('x')],
+                                     aa=[oa('var_att_a', 'this')]),
+                                  ov('var_B', dd=[od('t')])],
+                              aa=[oa('att_1', 4.3), oa('att_2', 4.7)])])
+#        print '\nTEST:\n', test_root
+#        print '\nSELF:\n', self.root
+        self.assertEqual(str(test_root), str(self.root))
+
+
 class Test_walk_group_objects(MixinTestSetup):
     def test_basic(self):
         walk_objs = list(walk_group_objects(self.root))
+        # NOTE: can't use set-equality here, as it test 'is' not '__eq__'.
         for elem in self.all_objs:
             self.assertIn(elem, walk_objs)
         for obj in walk_objs:
@@ -87,15 +128,82 @@ class Test_walk_group_objects(MixinTestSetup):
 
 
 class Test_group_path(MixinTestSetup):
-    def test_isolate(self):
-        self.assertEqual(group_path(self.attr_sub_var_A_A), 'var_att_a')
+    def test_isolate_and_attribute(self):
+        self.assertEqual('att_a', group_path(oa('att_a')))
 
-    def test_root(self):
-        self.assertEqual(group_path(self.root.variables['x']), '/x')
+    def test_root_and_variable(self):
+        g = og('', vv=[ov('var_test_xx')])
+        test_var = g.variables['var_test_xx']
+        self.assertEqual('/var_test_xx',
+                         group_path(test_var))
 
-    def test_subgroup(self):
-        self.assertEqual(group_path(self.subgroup.dimensions['x']),
-                         '/group_X/x')
+    def test_subgroup_and_dimension(self):
+        g = og('', gg=[og('sub_group', dd=[od('dim_q')])])
+        test_dim = g.groups['sub_group'].dimensions['dim_q']
+        self.assertEqual('/sub_group/dim_q',
+                         group_path(test_dim))
+
+
+class Test_find_named_definition(MixinTestSetup):
+    def check_all_in_str(self, str, matches):
+        for match in matches:
+            self.assertIn(match, str)
+
+    def test_type_bad(self):
+        g = og('')
+        with self.assertRaises(ValueError) as err_context:
+            fnd(g, 'name', type(None))
+        msg = err_context.exception.message
+        self.check_all_in_str(msg, ['<type \'NoneType\'>',
+                              'not recognised',
+                              'or not supported'])
+
+    def test_type_unsupported(self):
+        g = og('')
+        with self.assertRaises(ValueError) as err_context:
+            fnd(g, 'name', nco.Variable)
+        msg = err_context.exception.message
+        self.check_all_in_str(msg, ['ncobj.Variable',
+                              'not recognised',
+                              'or not supported'])
+
+    def test_root_none(self):
+        g = og('')
+        self.assertIsNone(fnd(g, 'dim_q', nco.Dimension))
+
+    def test_root_in(self):
+        g = og('', dd=[od('dim_q', 2)])
+        test_dim = g.dimensions['dim_q']
+        self.assertIs(fnd(g, 'dim_q', nco.Dimension),
+                      test_dim)
+
+    def test_root_notin(self):
+        g = og('', dd=[od('dim_q_NOT', 2)])
+        self.assertIsNone(fnd(g, 'dim_q', nco.Dimension))
+
+    def test_subgroup_none(self):
+        g = og('', gg=[og('sg')])
+        test_grp = g.groups['sg']
+        self.assertIsNone(fnd(test_grp, 'dim_q', nco.Dimension))
+
+    def test_subgroup_notin(self):
+        g = og('', gg=[og('sg', dd=[od('dim_q', 3)])])
+        test_grp = g.groups['sg']
+        self.assertIsNone(fnd(test_grp, 'dim_q_NOT', nco.Dimension))
+
+    def test_subgroup_in(self):
+        g = og('', gg=[og('sg', dd=[od('dim_q', 3)])])
+        test_grp = g.groups['sg']
+        test_dim = test_grp.dimensions['dim_q']
+        self.assertEquals(fnd(test_grp, 'dim_q', nco.Dimension),
+                          test_dim)
+
+    def test_subgroup_in_parent(self):
+        g = og('', gg=[og('sg')], dd=[od('dim_q', 3)])
+        test_grp = g.groups['sg']
+        test_dim = g.dimensions['dim_q']
+        self.assertEquals(fnd(test_grp, 'dim_q', nco.Dimension),
+                          test_dim)
 
 
 if __name__ == '__main__':
